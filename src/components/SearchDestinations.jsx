4c48766eddddfd056851f2316a34e4b46d9f5af8
Thiago Oliveira <77520434+Thiago-Batista-da-Silva-Oliveira@users.noreply.github.com>
2022-02-21 10:24:34 -0300

    Update SearchDestinations.jsx

--- src/components/SearchDestinations.jsx
+++ src/components/SearchDestinations.jsx
@@ -9,10 +9,23 @@ import {
   } from "@react-google-maps/api";
   import {useState} from 'react'
   import "../pages/MapPage.css"
+  import { Tooltip, IconButton, Box } from '@mui/material';
+  import CheckIcon from '@mui/icons-material/Check';
+  import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
+  import BoltIcon from '@mui/icons-material/Bolt';
 
-export const SearchDestinations = ({setDestinationValues, destinationValues,points,setPoints}) => {
+export const SearchDestinations = ({setDestinationValues,
+   destinationValues,
+   points,setPoints,
+   lastPriorityDestinationValues,
+   setLastPriorityDestinationValues,
+   priorityDestinationValues,
+   setPriorityDestinationValues
+  }) => {
     const [searchBox, setSearchBox] = useState(null)
     const [confirmed, setConfirmed] = useState(false) 
+    const [priority, setPriority] = useState(false) 
+    const [last, setLast] = useState(false) 
     const onLoadB = (ref) => {
         setSearchBox(ref);
       };
@@ -30,30 +43,56 @@ export const SearchDestinations = ({setDestinationValues, destinationValues,poin
        
       };
 
-      const handleClick = () => {
-       
+      const handleConfirm = () => {
         setDestinationValues([...destinationValues, {points}])
         setConfirmed(true)
-       
+      }
+
+      const handlePriority = () => {
+        setPriorityDestinationValues([...priorityDestinationValues, {points}])
+        setPriority(true)
+      }
+
+      const handleLast = () => {
+        setLastPriorityDestinationValues([...lastPriorityDestinationValues, {points}])
+        setLast(true)
       }
 
       
     
     return (
         <>
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <Box style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <Box sx={{display: 'flex'}}>
  <StandaloneSearchBox
     onLoad={onLoadB}
     onPlacesChanged={onPlacesChangedB}
    >
     <input
     className="addressField"
-    style= {{width: '305px'}}
+    style= {{width: '355px'}}
     placeholder="Digite o endereço"
   />
 </StandaloneSearchBox>
-  <button onClick={() => handleClick() } style={{background: confirmed ? 'green' : 'gray', border: 'none', outline: 'none', padding: '5px', cursor: 'pointer', borderRadius: '50%', color: 'white' }}><i class="fa-solid fa-check"></i></button>
-  </div>
+  
+ 
+  <Tooltip title="Confirmar">
+   <IconButton  disabled={last || priority}  style={{color: confirmed ? 'green' : 'black'}} onClick={() => handleConfirm() }>
+     <CheckIcon />
+   </IconButton>
+   </Tooltip>
+  <Tooltip title="Prioridade">
+   <IconButton disabled={confirmed || last} onClick={() => handlePriority()} style={{color: priority ? 'orange' : 'black'}}>
+     <BoltIcon />
+   </IconButton>
+  </Tooltip>
+  <Tooltip title="Último">
+   <IconButton  disabled={confirmed || priority} onClick={() => handleLast() } style={{color: last ? 'red' : 'black'}}>
+     <PriorityHighIcon />
+   </IconButton>
+  </Tooltip>
+    </Box>
+  </Box>
         </>
     )
 }
